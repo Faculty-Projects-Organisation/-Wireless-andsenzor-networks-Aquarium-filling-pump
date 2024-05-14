8a56c77c9c6423e82bab2817af510a06757d6dbe
Aleksander Kovač <aleksander.kovac97@hotmail.com>
2024-05-14 22:28:24 +0200

    Found a way to commit files.

---
+++ ultram/mqtt_config.h
@@ -0,0 +1,6 @@
+//Connect to LUCAMI MQTT server
+#define NODE = "/NODE03"
+#define BROKER_IP = "rlab.lucami.org"
+#define BROKER_USER = "lucmqtt"
+#define BROKER_PASS = "lucami2021"
+
---
+++ ultram/wifi_config.h
@@ -0,0 +1,10 @@
+//
+// Why this file?
+//
+// We all need to add our personal SSID/password to each ESP project but we
+// do not want that information pushed to Github. This file solves that
+// problem. Create an include/private_ssid_config.h file with the following two
+// definitions uncommented:
+//
+#define SSID "BrezzicnaInSenzorskaOmrezja"
+#define PASSWORD "Pr0ject!234"
