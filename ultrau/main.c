7d7ec47fe2d42af11d0bb1a416f479409222de78
Urban Poljsak <upoljs@hotmail.com>
2024-05-22 17:36:05 +0200

    uart dist or temp

--- ultrau/main.c
+++ ultrau/main.c
@@ -6,18 +6,22 @@
 #include <string.h>
 #include "task.h"
 
-#define US100_TX    4     // GPIO4 for TX (connect to RX pin of US-100)
-#define US100_RX    5     // GPIO5 for RX (connect to TX pin of US-100)
-#define BAUD_RATE   9600  // Baud rate for UART communication
+#define US100_TX 4
+#define US100_RX 5
+#define BAUD_RATE 9600
 
-void uart_write_bytes(const uint8_t *data, uint32_t length) {
-    for (uint32_t i = 0; i < length; i++) {
+void uart_write_bytes(const uint8_t *data, uint32_t length)
+{
+    for (uint32_t i = 0; i < length; i++)
+    {
         uart_putc(0, data[i]);
     }
 }
 
-void uart_read_bytes(uint8_t *data, uint32_t length) {
-    for (uint32_t i = 0; i < length; i++) {
+void uart_read_bytes(uint8_t *data, uint32_t length)
+{
+    for (uint32_t i = 0; i < length; i++)
+    {
         data[i] = uart_getc(0);
     }
 }
@@ -27,25 +31,51 @@ void ultrasonic_task(void *pvParameters)
     uint8_t txData = 0x55;
     uint8_t rxBuffer[2];
 
-    while (1) {
-        // Send command
+    uint8_t txTemp = 0x50;
+    uint8_t rxTBuffer[1];
+
+    while (1)
+    {
+        // distance
         uart_write_bytes(&txData, 1);
 
         vTaskDelay(pdMS_TO_TICKS(100));
 
-        // Read response (two bytes)
         uart_read_bytes(rxBuffer, 2);
 
-        // Combine the received bytes into a 16-bit value (assuming little-endian)
         uint16_t distance = (rxBuffer[1] << 8) | rxBuffer[0];
 
-        if (distance < 10000 || distance > 1) {
+        if (distance < 10000 || distance > 1)
+        {
             printf("Distance: %d mm\n", distance);
-        } else {
+        }
+        else
+        {
             printf("Measuring");
         }
 
-        vTaskDelay(pdMS_TO_TICKS(5000));
+        vTaskDelay(pdMS_TO_TICKS(100));
+
+        // temperature
+        uart_write_bytes(&txTemp, 1);
+
+        vTaskDelay(pdMS_TO_TICKS(50));
+
+        uart_read_bytes(rxTBuffer, 1);
+
+        uint16_t temperature = rxTBuffer[0];
+
+        if ((temperature > 1) && (temperature < 130))
+        {
+            temperature -= 45;
+            printf("Temperature: %d degrees\n", (temperature));
+        }
+        else
+        {
+            printf("Invalid temperature: %d\n", temperature);
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(2500));
     }
 }
 
@@ -54,12 +84,10 @@ void user_init(void)
 
     uart_set_baud(0, BAUD_RATE);
 
-
     i2c_init(0, US100_TX, US100_RX, I2C_FREQ_100K);
 
-    gpio_enable(US100_TX, GPIO_OUTPUT); // Set TX pin as output
-    gpio_enable(US100_RX, GPIO_INPUT);  // Set RX pin as input
+    gpio_enable(US100_TX, GPIO_OUTPUT);
+    gpio_enable(US100_RX, GPIO_INPUT);
 
     xTaskCreate(ultrasonic_task, "Ultrasonic Task", 256, NULL, 2, NULL);
-    
 }
